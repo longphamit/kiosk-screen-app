177c3d4ebc0b856c87ddb6bc4f4e4a0ab798abcf
phamlong992k@gmail.com <phamlong992k@gmail.com>
2022-06-05 01:12:14 +0700

    update template map

--- src/kiosks/map/index.jsx
+++ src/kiosks/map/index.jsx
@@ -1,8 +1,11 @@
 import * as React from 'react';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import MapGL from '@goongmaps/goong-map-react';
-import { Button, Col, Row } from 'antd';
+import { Avatar, Button, Card, Col, List, message, Row } from 'antd';
 import "./styles.css"
+import VirtualList from 'rc-virtual-list';
+const { Meta } = Card;
+const ContainerHeight = 400;
 const MapPage = () => {
   const [viewport, setViewport] = useState({
     width: '100%',
@@ -11,34 +14,184 @@ const MapPage = () => {
     longitude: -122.4376,
     zoom: 8
   });
-
+  const appendData = () => {
+    fetch('https://randomuser.me/api/?results=20&inc=name,gender,email,nat,picture&noinfo')
+      .then(res => res.json())
+      .then(body => {
+        setData(data.concat(body.results));
+        console.log(body)
+        message.success(`${body.results.length} more items loaded!`);
+      });
+  };
+  const [data, setData] = useState([]);
+  const onScroll = (e) => {
+    if (e.currentTarget.scrollHeight - e.currentTarget.scrollTop === ContainerHeight) {
+      appendData();
+    }
+  };
+  useEffect(() => {
+    appendData();
+  }, []);
   return (
     <>
       <Col xl={24} xs={24}>
-        <Row>
-          <Button>Home</Button>
-        </Row>
+        
         <Row>
           <Col xl={18} xs={24}>
-            <div style={{padding:20}}>
-            <MapGL
-              {...viewport}
-              className="map"
-              
-              onViewportChange={setViewport}
-              goongApiAccessToken={"GlVNPt2Vav2Z75sQm6lJ7XymStHLVD8UcWwhbWMn"}
-            />
+            <div style={{ padding: 20 }}>
+              <MapGL
+                {...viewport}
+                className="map"
+
+                onViewportChange={setViewport}
+                goongApiAccessToken={"GlVNPt2Vav2Z75sQm6lJ7XymStHLVD8UcWwhbWMn"}
+              />
             </div>
           </Col>
           <Col xl={6}>
-            <div style={{padding:20}}>
-              aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+            <div className='poi-address-text'>
+              <Row>
+                <Col span={2} style={{ textAlign: "center" }}>
+                  <img
+                    width="50%"
+                    id="market-poi-address-text"
+                    alt="example"
+                    src={require('../../assets/images/marker-1.png')}
+                  />
+                </Col>
+                <Col>
+                  36 Đ.Nguyễn Hữu Thọ, Tân Hưng, Quận 7, TP.Hồ Chí Minh
+                </Col>
+              </Row>
+
             </div>
-            <div style={{padding:20}}>
-              <h2>Gần đây</h2>
+            <div style={{ padding: 20 }}>
+              <h2>Near you</h2>
+              <VirtualList
+                data={data}
+                height={ContainerHeight}
+                itemHeight={47}
+                itemKey="email"
+                onScroll={onScroll}
+              >
+                {(item) => (
+                  <List.Item key={item.email}>
+                    <div className='poi-card-box'>
+                      <Row>
+                        <Col xl={6}>
+                          <img
+                            width="100%"
+                            alt="example"
+                            src={require('../../assets/images/restaurant.png')}
+                          />
+                        </Col>
+                        <Col xl={18}>
+                          <div style={{ marginLeft: 10 }}>
+                            <Meta title="Europe Street beat" description="www.instagram.com" />
+                          </div>
+                        </Col>
+                      </Row>
+                    </div>
+                  </List.Item>
+                )}
+              </VirtualList>
+
             </div>
           </Col>
         </Row>
+        <Row gutter={[16, 24]} style={{ margin: 10 }}>
+          <Col span={3}>
+            <div className='poi-category-card-box'>
+              <Row>
+                <Col span={2}>
+                  <img
+                    width="200%"
+                    alt="example"
+                    src={require('../../assets/images/restaurant-1.png')}
+                  />
+                </Col>
+                <Col span={22}>
+                  <div style={{ textAlign: "center" }}>
+                    <h2>Restaurant</h2>
+                  </div>
+                </Col>
+              </Row>
+            </div>
+          </Col>
+          <Col span={3}>
+            <div className='poi-category-card-box'>
+              <Row>
+                <Col span={2}>
+                  <img
+                    width="200%"
+                    alt="example"
+                    src={require('../../assets/images/coffee-1.png')}
+                  />
+                </Col>
+                <Col span={22}>
+                  <div style={{ textAlign: "center" }}>
+                    <h2>Coffee</h2>
+                  </div>
+                </Col>
+              </Row>
+            </div>
+          </Col>
+          <Col span={3}>
+            <div className='poi-category-card-box'>
+              <Row>
+                <Col span={2}>
+                  <img
+                    width="200%"
+                    alt="example"
+                    src={require('../../assets/images/shopping-1.png')}
+                  />
+                </Col>
+                <Col span={22}>
+                  <div style={{ textAlign: "center" }}>
+                    <h2>Shopping</h2>
+                  </div>
+                </Col>
+              </Row>
+            </div>
+          </Col>
+          <Col span={3}>
+            <div className='poi-category-card-box'>
+              <Row>
+                <Col span={2}>
+                  <img
+                    width="200%"
+                    alt="example"
+                    src={require('../../assets/images/fuel-1.png')}
+                  />
+                </Col>
+                <Col span={22}>
+                  <div style={{ textAlign: "center" }}>
+                    <h2>Fuel</h2>
+                  </div>
+                </Col>
+              </Row>
+            </div>
+          </Col>
+          <Col span={3}>
+            <div className='poi-category-card-box'>
+              <Row>
+                <Col span={2}>
+                  <img
+                    width="200%"
+                    alt="example"
+                    src={require('../../assets/images/hospital.png')}
+                  />
+                </Col>
+                <Col span={22}>
+                  <div style={{ textAlign: "center" }}>
+                    <h2>Hospital</h2>
+                  </div>
+                </Col>
+              </Row>
+            </div>
+          </Col>
+        </Row>
+
       </Col>
     </>
 
