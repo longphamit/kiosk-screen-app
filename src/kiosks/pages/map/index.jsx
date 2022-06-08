98b17644089903d1db755626f1f539b752d35390
phamlong992k@gmail.com <phamlong992k@gmail.com>
2022-06-09 04:58:19 +0700

    marker current location

--- src/kiosks/pages/map/index.jsx
+++ src/kiosks/pages/map/index.jsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import { useState, useEffect } from 'react';
-import MapGL from '@goongmaps/goong-map-react';
+import ReactMapGL, { Marker, Popup, ScaleControl } from '@goongmaps/goong-map-react';
 import { Avatar, Button, Card, Col, List, message, Row } from 'antd';
 import "./styles.css"
 import VirtualList from 'rc-virtual-list';
@@ -8,15 +8,24 @@ import { useGeolocated } from "react-geolocated";
 import { useNavigate } from 'react-router-dom';
 const { Meta } = Card;
 const ContainerHeight = 400;
+const scaleControlStyle = {
+  left: 20,
+  bottom: 100
+};
 const MapPage = () => {
   const navigate = useNavigate()
+  const [currentLocation, setCurrentLocation] = useState({
+    longitude: 105.7982323,
+    latitude: 21.0136133
+  });
   const [viewport, setViewport] = useState({
     width: '100%',
     height: 600,
-    latitude: 21.0136133,
-    longitude: 105.7982323,
-    zoom: 8
+    latitude: currentLocation.latitude,
+    longitude: currentLocation.longitude,
+    zoom: 13
   });
+
   const test = useGeolocated({
     positionOptions: {
       enableHighAccuracy: false,
@@ -41,12 +50,18 @@ const MapPage = () => {
 
     }
   };
+  const setLocationViewPort = () => {
+    navigator.geolocation.getCurrentPosition((position) => {
+      console.log(position.coords.latitude)
+      console.log(position.coords.longitude)
+      setCurrentLocation({ ...currentLocation, latitude: position.coords.latitude, longitude: position.coords.longitude })
+      setViewport({ ...viewport, latitude: position.coords.latitude, longitude: position.coords.longitude })
+    });
+  }
+
   useEffect(() => {
     appendData();
-    navigator.geolocation.getCurrentPosition((position) => {
-      console.log(`lat=${position.coords.latitude}&lon=${position.coords.longitude}`)
-      setViewport({ ...viewport, latitude: position.coords.latitude, longitude: position.coords.longitude,zoom:13 })
-    });
+    setLocationViewPort()
   }, []);
   return (
     <>
@@ -55,13 +70,24 @@ const MapPage = () => {
         <Row>
           <Col xl={18} xs={24}>
             <div style={{ padding: 20 }}>
-              <MapGL
+              <ReactMapGL
                 {...viewport}
                 className="map"
-
                 onViewportChange={setViewport}
                 goongApiAccessToken={"GlVNPt2Vav2Z75sQm6lJ7XymStHLVD8UcWwhbWMn"}
-              />
+              >
+                <ScaleControl maxWidth={100} unit="metric" style={scaleControlStyle} />
+                <Marker latitude={currentLocation.latitude} longitude={currentLocation.longitude} offsetLeft={-20} offsetTop={-10}>
+                  <Col>
+                    <div><img
+                      id="marker"
+                      alt="example"
+                      src={require('../../../assets/images/pin-1.png')}
+                    /></div>
+                  </Col>
+
+                </Marker>
+              </ReactMapGL >
             </div>
           </Col>
           <Col xl={6}>
@@ -70,7 +96,7 @@ const MapPage = () => {
                 <Col span={2} style={{ textAlign: "center" }}>
                   <img
                     width="50%"
-                    id="market-poi-address-text"
+                    id="marker-poi-address-text"
                     alt="example"
                     src={require('../../../assets/images/marker-1.png')}
                   />
