98733a74567fa0b0e31546f4197040e35ca021f3
hhtin <tinhuynhhuu250500@gmail.com>
2022-07-12 02:24:00 +0700

    CPF-251 finish

---
+++ src/kiosks/pages/app_list/iframeInterface.jsx
@@ -0,0 +1,95 @@
+import {
+  Layout,
+  Menu,
+  Space,
+  Dropdown,
+  Row,
+  Col,
+  Modal,
+  Form,
+  Button,
+} from "antd";
+import { ArrowLeftOutlined, DownOutlined } from "@ant-design/icons";
+import "./styles.css";
+import { useState, useEffect } from "react";
+import Iframe from "react-iframe";
+import { useNavigate, useSearchParams } from "react-router-dom";
+import { formItemLayout } from "../../layouts/form_layout";
+
+const { Header, Content, Sider } = Layout;
+
+const IframeInterface = () => {
+  const [searchParams, setSearchParams] = useSearchParams();
+  const [link, setLink] = useState(null);
+  const [id, setId] = useState(null);
+  const [form] = Form.useForm();
+
+  let navigate = useNavigate();
+  const onNavigate = (url) => {
+    navigate(url);
+  };
+
+  const getInitValue = async () => {
+    let link = searchParams.get("link");
+    let id = searchParams.get("id");
+    if (link == null) {
+      onNavigate("/././unauth");
+      return;
+    }
+    if (id == null) {
+      onNavigate("/././unauth");
+      return;
+    }
+    setId(id);
+    setLink(link);
+  };
+  useEffect(async () => {
+    await getInitValue();
+  }, []);
+  return (
+    <Layout>
+      <Header className="header">
+        <Row>
+          <Col span={10}>
+            {" "}
+            <Button
+              type="primary"
+              danger
+              onClick={() => {
+                onNavigate({
+                  pathname: "/./app-list?id=" + id,
+                });
+              }}
+            >
+              Back
+            </Button>
+          </Col>
+        </Row>
+      </Header>
+      <Row justify="center" align="middle" style={{ height: "100vh" }}>
+        <Layout style={{ height: "100%", width: "100%" }}>
+          <Content
+            className="site-layout-background"
+            style={{
+              padding: 24,
+              margin: 0,
+              width: "100%",
+              height: "100%",
+            }}
+          >
+            <Iframe
+              url={link}
+              width="100%"
+              height="100%"
+              id="myId"
+              className="myClassname"
+              display="initial"
+              position="relative"
+            />
+          </Content>
+        </Layout>
+      </Row>
+    </Layout>
+  );
+};
+export default IframeInterface;
